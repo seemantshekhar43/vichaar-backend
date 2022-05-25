a6e04f4fdcb9a05b0bd78fa0b2f22a217a5b4991
seemants43 <seemants@tekion.com>
2022-05-25 13:47:58 +0530

    initial commit

---
+++ src/main/java/com/seemantshekhar/vichhar/VichharApplication.java
@@ -0,0 +1,13 @@
+package com.seemantshekhar.vichhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VichharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VichharApplication.class, args);
+    }
+
+}
